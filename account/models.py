d9ce505f2a87e9b3884211f850333a47527ed35b
CYJhub <yejee1018@naver.com>
2023-03-31 23:42:52 +0900

    feat: account 생성

--- account/models.py
+++ account/models.py
@@ -3,12 +3,36 @@ from django.contrib.auth.models import AbstractBaseUser, UserManager
 from core.models import TimestampedModel
 import uuid
 
-class School(models.Model):
+class School(TimestampedModel):
     school_name = models.CharField(max_length=100)
 
     def __str__(self):
         return self.school_name
+class UserManager(TimestampedModel):
+    # 필수로 필요한 데이터를 선언
+    def create_user(self, username, password):
+        if not username:
+            raise ValueError('Users must have an username')
+        if not password:
+            raise ValueError('Password must have and password')
 
+        user = self.model(
+            username=username,
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    # python manage.py createsuperuser 사용 시 해당 함수가 사용됨
+    def create_superuser(self, username, email, password=None):
+        user = self.create_user(
+            email,
+            username=username,
+            password=password
+        )
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
 class User(AbstractBaseUser,TimestampedModel):
     # 기본키
     user_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
@@ -27,10 +51,6 @@ class User(AbstractBaseUser,TimestampedModel):
 
     school_id = models.ForeignKey(School, on_delete=models.PROTECT)
 
-    # 어드민 계정을 만들 때 입력받을 정보 ex) email
-    # 사용하지 않더라도 선언이 되어야함
-    # USERNAME_FIELD와 비밀번호는 기본적으로 포함되어있음
-
     REQUIRED_FIELDS = ['username']  # 필수로 값을 받아야하는 필드
     USERNAME_FIELD = 'username'
     # custom user 생성 시 필요
@@ -39,20 +59,4 @@ class User(AbstractBaseUser,TimestampedModel):
     def __str__(self):
         return self.name
 
-    # 로그인 사용자의 특정 테이블의 crud 권한을 설정, perm table의 crud 권한이 들어간다.
-    # admin일 경우 항상 True, 비활성 사용자(is_active=False)의 경우 항상 False
-    # 일반적으로 선언만 해두고 건들지않는다
-    def has_perm(self, perm, obj=None):
-        return True
-
-    # 로그인 사용자의 특정 app에 접근 가능 여부를 설정, app_label에는 app 이름이 들어간다.
-    # admin일 경우 항상 True, 비활성 사용자(is_active=False)의 경우 항상 False
-    # 일반적으로 선언만 해두고 건들지않는다
-    def has_module_perms(self, app_label):
-        return True
-
-    # admin 권한 설정
-    @property
-    def is_staff(self):
-        return self.is_admin
 
