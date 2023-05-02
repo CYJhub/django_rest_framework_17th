4fd0d98745373c2874d8c53575f32577205fe95f
CYJhub <yejee1018@naver.com>
2023-05-02 21:39:55 +0900

    feat: base에 권한 설정 추가

--- django_rest_framework_17th/settings/base.py
+++ django_rest_framework_17th/settings/base.py
@@ -54,6 +54,11 @@ INSTALLED_APPS = [
 AUTH_USER_MODEL = 'account.User'
 
 REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': (
+        # 'rest_framework.permissions.IsAuthenticated', # 인증된 사용자만 접근
+        # 'rest_framework.permissions.IsAdminUser', # 관리자만 접근
+        'rest_framework.permissions.AllowAny', # 누구나 접근
+    ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     )
